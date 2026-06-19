8c6e7df88f6e53614dc8f1915331c21af04a1061
ravinu177 <ravinulakshan18@gmail.com>
2026-06-19 16:29:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
     storeWriteInterval: 10000,
     description: "This is a bot for managing group commands and automating tasks.",
     version: "3.0.7",
-    SESSION_ID: 'RAVI-MD~unb_e6b0_d758_ca7e_a257_1ec0_37e5'
+    SESSION_ID: process.env.SESSION_ID || 'RAVI-MD~unb_e6b0_d758_ca7e_a257_1ec0_37e5'
 };
 
 module.exports = settings;
