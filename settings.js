aa86e066ed1dae1d5f9d9cb64039d1064f9a64a0
ravinu177 <ravinulakshan18@gmail.com>
2026-06-19 10:30:22 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  maxStoreMessages: 20, 
-  storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+    packname: 'ravi-md Bot',
+    author: '‎',
+    botName: "ravi-md Bot",
+    botOwner: 'Professor', 
+    ownerNumber: '94768223718', 
+    giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+    commandMode: "public",
+    maxStoreMessages: 20,
+    storeWriteInterval: 10000,
+    description: "This is a bot for managing group commands and automating tasks.",
+    version: "3.0.7",
+    SESSION_ID: process.env.SESSION_ID
 };
 
 module.exports = settings;
