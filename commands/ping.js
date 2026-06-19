d7362b91d15f13ae440900572e9765fac2dec5bf
ravinu177 <ravinulakshan18@gmail.com>
2026-06-19 14:03:05 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -29,7 +29,7 @@ async function pingCommand(sock, chatId, message) {
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
         const botInfo = `
-┏━━〔 🤖 𝐊𝐧𝐢𝐠𝐡𝐭𝐁𝐨𝐭-𝐌𝐃 〕━━┓
+┏━━〔 🤖 ravi--𝐌𝐃 〕━━┓
 ┃ 🚀 Ping     : ${ping} ms
 ┃ ⏱️ Uptime   : ${uptimeFormatted}
 ┃ 🔖 Version  : v${settings.version}
